fc97ce7581d8b628526dfc4a0133d90beebf368f
imhaseeb95 <145844952+imhaseeb95@users.noreply.github.com>
2024-01-10 23:50:22 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,190 +1,184 @@
-'use strict';
+// ========================
+// 	 About Slider
+// ========================
 
-// ===============
-// 	slider
-// ===============
-
-$('.sweets-slider').slick({
-	dots: true,
-	infinite: true,
-	speed: 300,
-	slidesToShow: 3,
-	slidesToScroll: 1,
-	arrows: false,
-	responsive: [
-		{
-			breakpoint: 1024,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
-				infinite: true,
-				dots: true,
-			},
-		},
-		{
-			breakpoint: 600,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2,
-			},
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-			},
-		},
-	],
+$(".aboutSlider").slick({
+  autoplay: true,
+  infinite: true,
+  arrows: false,
+  dots: false,
+  speed: 300,
+  fade: true,
+  cssEase: "linear",
+  slidesToShow: 1,
+  slidesToScroll: 1,
 });
 
-$('.testimonial-slider').slick({
-	dots: true,
-	infinite: true,
-	speed: 300,
-	slidesToShow: 3,
-	slidesToScroll: 1,
-	arrows: false,
-	responsive: [
-		{
-			breakpoint: 1024,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
-				infinite: true,
-				dots: true,
-			},
-		},
-		{
-			breakpoint: 600,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2,
-			},
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-			},
-		},
-	],
+// ========================
+// 	 About2 Slider
+// ========================
+
+$(".about2-slider").slick({
+  arrows: false,
+  dots: true,
+  centerMode: true,
+  centerPadding: "350px",
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
 
-// ===============
-// 	Fancy Box
-// ===============
-
-Fancybox.bind('[data-fancybox]');
-
-// ===============
-// 	Accordian
-// ===============
-
-const accordian = document.querySelectorAll('.contentBx');
-
-for (let i = 0; i < accordian.length; i++) {
-	accordian[i].addEventListener('click', function () {
-		this.classList.toggle('active');
-	});
-}
-
-// ===============
-// 	Header
-// ===============
+// ========================
+// 	 Header scroll
+// ========================
 
 // var navBar = document.querySelector("header");
-// var headerScroll = document.querySelector("header.sec-header");
-// var headerY = 100;
+var headerScroll = document.querySelector("header.sec-header");
+var headerY = 100;
 
-// window.addEventListener("scroll", function () {
-//   var haederTop = window.scrollY;
-//   if (haederTop > headerY) {
-//     headerScroll.classList.add("nav-bar-active");
-//   } else {
-//     headerScroll.classList.remove("nav-bar-active");
-//   }
-// });
+window.addEventListener("scroll", function () {
+  var haederTop = window.scrollY;
+  if (haederTop > headerY) {
+    headerScroll.classList.add("nav-bar-active");
+  } else {
+    headerScroll.classList.remove("nav-bar-active");
+  }
+});
 
 // ========================
-// 	Home counter Scroll
+// 	 Scroll Top
 // ========================
 
-const counters = document.querySelectorAll('.count');
-const speed = 5;
+var one = document.querySelector(".scroll");
+var arrowY = 100;
 
+window.addEventListener("scroll", function () {
+  // console.log(window.scrollY)
+  var scrollTop = window.scrollY;
+  if (scrollTop > arrowY) {
+    one.classList.remove("actvie-arrow");
+  } else {
+    one.classList.add("actvie-arrow");
+  }
+});
 
+// =====================
+// 	Slick slider
+// =====================
 
-var counterHeight = document.querySelector('.products');
-var heightCounter = 1940;
-//counterHeight.offsetTop
+$(".slider").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  dots: true,
+  arrows: false,
+});
 
+// =====================
+// 	accordian
+// =====================
 
- window.addEventListener('scroll', function(){
-  var scrollTop = window.scrollY 
-  
-  if(scrollTop > heightCounter){
-    counters.forEach(myCounter => {
-      myCounter.innerText = '0';
-    
-      const updateCounter = ()=>{
-    
-      let myNum = +myCounter.getAttribute('data-target');
-      let myTarget = +myCounter.innerText;
-    
-      let newSpeed = myNum / speed; 
-    
-      if(myTarget < myNum){
-        myCounter.innerText = `${Math.ceil(myTarget + newSpeed)}`;
-        setTimeout(updateCounter, 1)
-      }else{
-        myCounter.innerText = myNum
-      }
+var acc = document.getElementsByClassName("btn-acc");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
-    updateCounter()
+  });
+}
+
+// ========================
+// 	counter Scroll
+// ========================
+
+const counters = document.querySelectorAll(".counter-run");
+
+const speed = 200;
+
+var testDiv = document.querySelector("#counter-sec");
+var b = testDiv.offsetTop;
+console.log(b);
+//3000
+
+window.addEventListener("scroll", function () {
+  var scrollY = window.scrollY;
+  if (scrollY > b) {
+    counters.forEach((counter) => {
+      counter.innerText = "0";
+      const updateNum = () => {
+        const value = +counter.getAttribute("animate");
+        const data = +counter.innerText;
+
+        const time = value / speed;
+        if (data < value) {
+          counter.innerText = `${Math.ceil(data + time)}`;
+          setTimeout(updateNum, 100);
+        } else {
+          counter.innerText = value;
+        }
+      };
+      updateNum();
     });
   }
+});
 
-})
+// ===========================
+// 	About One counter Scroll
+// ===========================
 
+const counterOne = document.querySelectorAll(".counter2");
+const speed2 = 200;
 
-// ========================
-// 	About counter Scroll
-// ========================
+const countDiv = document.querySelector(".counter2-container");
+const lengthDiv = 500;
 
-const clientsCounter = document.querySelectorAll('.counter')
-const speedTime = 20;
+window.addEventListener("scroll", function () {
+  var checkLength = window.scrollY;
+  if (checkLength > lengthDiv) {
+    counterOne.forEach((count) => {
+      count.innerText = "0";
 
-const scrollHeight = document.querySelector('.clients-sec');
-const scrollTopHeight = 900 
-console.log(scrollTopHeight);
+      var counterFunc = () => {
+        var val = +count.getAttribute("data");
+        var data2 = +count.innerText;
+        var countDown = val / speed2;
 
-
-
-
-window.addEventListener('scroll', function(){
-  let topScroll = window.scrollY;
-
-  if(topScroll > scrollTopHeight){
-    clientsCounter.forEach((element)=>{
-      element.innerText = '0';
-    
-      const updateNum = ()=> {
-        let clietNum = +element.getAttribute('data-count');
-      let clientTarget = +element.innerText
-    
-      const numTime =  clietNum / speedTime
-    
-      if(clietNum > clientTarget){
-        element.innerText = `${Math.ceil(clientTarget + numTime)}`
-        setTimeout(updateNum, 1)
-      }
-    }
-    updateNum()
-    
-    })
+        if (data2 < val) {
+          count.innerText = `${Math.ceil(data2 + countDown)}`;
+          setTimeout(counterFunc, 100);
+        } else {
+          count.innerText = val;
+        }
+      };
+      counterFunc();
+    });
   }
-
-
-})
+});
